c4033530ae16c55af1fec06a2b26b325b3ecd103
hwajj <azamwara@gmail.com>
2024-08-28 14:45:34 +0900

    [fix] tailwind 관련 에러처리
    - classnames 추가

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useAtom } from "jotai";
 import { userAtom } from "@/atoms/userAtom.js";
+import classNames from "classnames";
 
 const getChipStyle = (color) => {
   switch (color) {
@@ -45,7 +46,7 @@ const Board = ({
 
   myTeamRef.current =
     players.find((player) => player.userId === user.uid)?.team || null;
-  console.log(myTeamRef.current);
+
   useEffect(() => {
     const changedPositionsTemp = [];
     const newHighlightedPositions = {};
@@ -102,27 +103,30 @@ const Board = ({
   };
 
   const isPlaceable = (card, rowIndex, colIndex) => {
+    const teamColorClass = `bg-${myTeamRef.current}-200`;
+
     if (clickedCard?.includes("J")) {
       if (clickedCard.includes("♥") || clickedCard.includes("♠")) {
         // 하트 또는 스페이드 J카드: 상대팀의 칩만 제거 가능
-        return board[rowIndex][colIndex].occupiedColor &&
-          board[rowIndex][colIndex].occupiedColor !== myTeamRef.current
-          ? `bg-${myTeamRef.current}-200`
-          : "";
+        return classNames({
+          [teamColorClass]:
+            board[rowIndex][colIndex].occupiedColor &&
+            board[rowIndex][colIndex].occupiedColor !== myTeamRef.current,
+        });
       } else if (clickedCard.includes("♦") || clickedCard.includes("♣")) {
         // 다이아 또는 클럽 J카드: 빈 칸에만 놓을 수 있음
-        return board[rowIndex][colIndex].occupiedColor === ""
-          ? `bg-${myTeamRef.current}-200`
-          : "";
+        return classNames({
+          [teamColorClass]: board[rowIndex][colIndex].occupiedColor === "",
+        });
       }
     } else if (clickedCard === card.value) {
-      return `bg-${myTeamRef.current}-200 transition-colors `;
+      return classNames(`${teamColorClass} transition-colors`);
     }
     return "";
   };
 
   return (
-    <div className="grid items-center justify-center rounded-sm mx-4 grid-rows-10  gap-0 text-[.7rem] xs:text-[.9rem] sm:text-lg  lg:text-xl">
+    <div className="grid items-center justify-center rounded-sm mx-4 grid-rows-10 gap-0 text-[.7rem] xs:text-[.9rem] sm:text-lg lg:text-xl">
       {board?.map((row, rowIndex) => (
         <div
           key={rowIndex}
@@ -139,24 +143,33 @@ const Board = ({
             return (
               <div
                 key={colIndex}
-                className={`no-select relative border-[.09rem] overflow-hidden border-gray-300
-                 w-[1.7rem] h-[1.7rem]  xs:w-[2rem] xs:h-[2rem] sm:w-[2.5rem] sm:h-[2.5rem] lg:h-[3rem] rounded-sm
-                  ${placeableClass}
-                  ${isSequence(rowIndex, colIndex) ? "bg-yellow-300" : ""}   
-                  ${getPlacedChipHighlight(rowIndex, colIndex)}
-                cursor-pointer flex items-center justify-center`}
+                className={classNames(
+                  "no-select relative border-[.09rem] overflow-hidden border-gray-300",
+                  "w-[1.7rem] h-[1.7rem] xs:w-[2rem] xs:h-[2rem] sm:w-[2.5rem] sm:h-[2.5rem] lg:h-[3rem] rounded-sm",
+                  placeableClass,
+                  {
+                    "bg-yellow-300": isSequence(rowIndex, colIndex),
+                  },
+                  getPlacedChipHighlight(rowIndex, colIndex),
+                  "cursor-pointer flex items-center justify-center",
+                )}
                 onClick={() =>
                   handleCardClick(card.value, [rowIndex, colIndex])
                 }
               >
                 <span
-                  className={`${textColor}  ${card.value === "Joker" && "bg-gray-400 w-full h-full"} `}
+                  className={classNames(textColor, {
+                    "bg-gray-400 w-full h-full": card.value === "Joker",
+                  })}
                 >
                   {card.value !== "Joker" ? card.value : ""}
                 </span>
                 {/* 칩을 표현하는 원(circle) */}
                 <div
-                  className={`absolute w-4 h-4 lg:w-5 lg:h-5 rounded-full ${getChipStyle(card.occupiedColor)}`}
+                  className={classNames(
+                    "absolute w-4 h-4 lg:w-5 lg:h-5 rounded-full",
+                    getChipStyle(card.occupiedColor),
+                  )}
                   style={{
                     top: "50%",
                     left: "50%",
